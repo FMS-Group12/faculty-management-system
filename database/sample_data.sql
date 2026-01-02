c45a2f4e34a5655c7b1f9b35036e5b2087a53c43
Sandamini-Madurawinda <maduraw-cs22060@stu.kln.ac.lk>
2026-01-02 22:38:39 +0530

    Update sample_data.sql

--- database/sample_data.sql
+++ database/sample_data.sql
@@ -8,13 +8,13 @@ INSERT INTO departments (name, hod, no_of_staff) VALUES ('Computer Systems Engin
 
 INSERT INTO departments (name, hod, no_of_staff) VALUES ('Applied Computing', 'Dr. L.S.I.Liyanage', 28);
 
-INSERT INTO degrees (degree, department_id, no_of_students) VALUES ('BSc CS', 1, 280);
+INSERT INTO degrees (degree,department_name, no_of_students) VALUES ('BSc CS', 'Software Engineering', 280);
 
-INSERT INTO degrees (degree, department_id, no_of_students) VALUES ('BICT', 3, 360);
+INSERT INTO degrees (degree, department_name, no_of_students) VALUES ('BICT','Applied Computing' , 360);
 
-INSERT INTO degrees (degree, department_id, no_of_students) VALUES ('BET', 2, 320);
+INSERT INTO degrees (degree, department_name, no_of_students) VALUES ('BET','Computer Systems Engineering' , 320);
 
-INSERT INTO degrees (degree, department_id, no_of_students) VALUES ('BST', 2, 300);
+INSERT INTO degrees (degree, department_name, no_of_students) VALUES ('BST', 'Computer Systems Engineering', 300);
 
 INSERT INTO students (student_id, fullname, email, mobile_no, degree_id, user_id) VALUES ('CS/2022/060','Sandamini Madurawinda','sandamini@gmail.com','0712345678',1,1);
     
@@ -23,6 +23,7 @@ INSERT INTO lecturers(fullname, email, mobile_no, department_id, user_id) VALUES
 INSERT INTO courses(course_code, course_name, credits, lecturer_id) VALUES ('CSCI 21052','OOP',2,1);
 
 INSERT INTO enrollments(student_id, course_code, grade) VALUES ('CS/2022/060','CSCI 21052','A');
+
 
 
 
