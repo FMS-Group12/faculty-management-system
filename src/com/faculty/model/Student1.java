f697e2107b552601cbaa7c00336e3a65c225ea67
Janithi-Leelananda <janithileelananda22@gmail.com>
2026-01-05 00:03:26 +0530

    Create Student1.java

---
+++ src/com/faculty/model/Student1.java
@@ -0,0 +1,18 @@
+public class Student1 {
+    public String student_id;
+    public String fullname;
+    public String email;
+    public String mobile_no;
+    public String degree_id;
+    public String user_id;
+
+    public Student1(String student_id, String fullname, String email, String mobile_no, String degree_id, String user_id) {
+        this.student_id = student_id;
+        this.fullname = fullname;
+        this.email = email;
+        this.mobile_no = mobile_no;
+        this.degree_id = degree_id;
+        this.user_id = user_id;
+    }
+}
+
