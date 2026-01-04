abfa4b20bcdff6f204d2fee7b2595a9c32e60e23
Sandamini-Madurawinda <maduraw-cs22060@stu.kln.ac.lk>
2026-01-04 12:47:06 +0530

    Update sample_data.sql

--- database/sample_data.sql
+++ database/sample_data.sql
@@ -18,11 +18,12 @@ INSERT INTO degrees (degree, department_name, no_of_students) VALUES ('BST', 'Co
 
 INSERT INTO students (student_id, fullname, email, mobile_no, degree_id, user_id) VALUES ('CS/2022/060','Sandamini Madurawinda','sandamini@gmail.com','0712345678',1,1);
     
-INSERT INTO lecturers(fullname, email, mobile_no, department_id, user_id) VALUES ('Kamal Silva','silva@gmail.com','0709876543',3,2);
+INSERT INTO lecturers(fullname, email, mobile_no, department_id,courses, user_id) VALUES ('Kamal Silva','silva@gmail.com','0709876543',3,'ETEC21062',2);
 
 INSERT INTO courses(course_code, course_name, credits, lecturer_id) VALUES ('CSCI 21052','OOP',2,1);
 
 INSERT INTO enrollments(student_id, course_code, grade) VALUES ('CS/2022/060','CSCI 21052','A');
+
 
 
 
