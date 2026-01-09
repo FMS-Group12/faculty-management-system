3286d132d1e48d5e4ae1a65e129bf059d0e67016
Nadara Jayasinghe <jayasin-cs22029@stu.kln.ac.lk>
2026-01-09 21:56:04 +0530

    Delete src/com/faculty/main/MainDepartment.java

--- src/com/faculty/main/MainDepartment.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.SwingUtilities;
-
-public class MainDepartment {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            new DepartmentDashboardView().setVisible(true);
-        });
-    }
-}
