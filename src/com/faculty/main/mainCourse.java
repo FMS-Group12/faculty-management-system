8ac14466124466e303c5b21e19c3c2e53399116a
Nadara Jayasinghe <jayasin-cs22029@stu.kln.ac.lk>
2026-01-09 21:56:59 +0530

    Delete src/com/faculty/main/mainCourse.java

--- src/com/faculty/main/mainCourse.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.*;
-
-public class mainCourse {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new CourseDashboardView().setVisible(true));
-    }
-}
