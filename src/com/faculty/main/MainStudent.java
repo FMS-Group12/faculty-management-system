6f19028e0b58673a9c49430fedce798debe809a3
Nadara Jayasinghe <jayasin-cs22029@stu.kln.ac.lk>
2026-01-09 21:58:27 +0530

    Delete src/com/faculty/main/MainStudent.java

--- src/com/faculty/main/MainStudent.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.SwingUtilities;
-
-public class MainStudent {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            new StudentDashboardView().setVisible(true);
-        });
-    }
-}
