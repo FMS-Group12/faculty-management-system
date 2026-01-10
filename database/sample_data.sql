db1edb09ae0b10d1dfbba63b963fefd094a9d15b
Sandamini-Madurawinda <maduraw-cs22060@stu.kln.ac.lk>
2026-01-10 18:53:14 +0530

    Update sample_data.sql

--- database/sample_data.sql
+++ database/sample_data.sql
@@ -1,8 +1,8 @@
-INSERT INTO users (username, password, role) VALUES ("CS/2022/060","Sanda123","STUDENT");
+INSERT INTO users (username, password, role) VALUES ("CS/01","Stu01","STUDENT");
 
-INSERT INTO users (username, password, role) VALUES ("Silva","Silva123","LECTURER");
+INSERT INTO users (username, password, role) VALUES ("LC/01","Lec01","LECTURER");
 
-INSERT INTO users( username, password, role) VALUES ('AD/2022/00','Admin123','ADMIN');
+INSERT INTO users( username, password, role) VALUES ('AD/00','Admin00','ADMIN');
 
 INSERT INTO departments (name, hod, no_of_staff) VALUES ('Software Engineering', 'Dr.S.P.Kasthuri Arachchi', 25);
 
@@ -18,13 +18,14 @@ INSERT INTO degrees (degree, department_name, no_of_students) VALUES ('BET','Com
 
 INSERT INTO degrees (degree, department_name, no_of_students) VALUES ('BST', 'Computer Systems Engineering', 300);
 
-INSERT INTO students (student_id, fullname, email, mobile_no, degree_id, user_id) VALUES ('CS/2022/060','Sandamini Madurawinda','sandamini@gmail.com','0712345678',1,1);
+INSERT INTO students (student_id, fullname, email, mobile_no, degree_id, user_id) VALUES ('CS/01','Sanduni Perera','sanduni@gmail.com','0712345678',1,1);
     
 INSERT INTO lecturers(fullname, email, mobile_no, department_id,courses, user_id) VALUES ('Kamal Silva','silva@gmail.com','0709876543',3,'ETEC21062',2);
 
 INSERT INTO courses(course_code, course_name, credits, lecturer_id) VALUES ('CSCI 21052','OOP',2,1);
 
-INSERT INTO enrollments(student_id, course_code, grade) VALUES ('CS/2022/060','CSCI 21052','A');
+INSERT INTO enrollments(student_id, course_code, grade) VALUES ('CS/01','CSCI 21052','A');
+
 
 
 
